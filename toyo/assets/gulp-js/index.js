2868a409bea7ed0d29ef52aafdc60f448661762e
MHon Romero <sales.ibitsolutions@gmail.com>
2018-10-06 19:22:24 +0800

    Toyo Tires Website Sub Folder v1.0.0

--- toyo/assets/gulp-js/index.js
+++ toyo/assets/gulp-js/index.js
@@ -1,3 +1,5 @@
+
+//Asynchronus Background image loading
 var img1 = document.createElement("img");
 
 $('body').css('background-image', 'url(assets/img/bg-min.jpg)');
@@ -6,3 +8,5 @@ $(img1).attr('src', 'assets/img/bg.jpg').on('load', function() {
    $(this).remove(); // prevent memory leaks as @benweet suggested
    $('body').css('background-image', 'url(assets/img/bg.jpg)');
 });
+
+new LazyLoad();
--- toyo/assets/js/index.js
+++ toyo/assets/js/index.js
@@ -1,1 +1,1 @@
-var img1=document.createElement("img");$("body").css("background-image","url(assets/img/bg-min.jpg)"),$(img1).attr("src","assets/img/bg.jpg").on("load",function(){$(this).remove(),$("body").css("background-image","url(assets/img/bg.jpg)")});
+var img1=document.createElement("img");$("body").css("background-image","url(assets/img/bg-min.jpg)"),$(img1).attr("src","assets/img/bg.jpg").on("load",function(){$(this).remove(),$("body").css("background-image","url(assets/img/bg.jpg)")}),new LazyLoad;
