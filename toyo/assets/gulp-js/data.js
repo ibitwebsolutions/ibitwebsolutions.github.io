668374fb842bf5ae36cc257dfa035a778b3ff4e9
MHon Romero <sales.ibitsolutions@gmail.com>
2018-10-12 12:00:42 +0800

    Dealers Page Map update

---
+++ toyo/assets/gulp-js/data.js
@@ -0,0 +1,17 @@
+var dealers = [
+			    {"dealer":"Dax Marketing",
+				 "add":"1036 Edsa, Pasay City",
+				 "coor":"14.5387929,121.0128582",
+				 "phone":"(02) 851-0849"
+				},
+			    {"dealer":"Bens Ok Cars Accessories",
+				 "add":"73 Banawe Cor. L. Roxas St., Quezon City",
+				 "coor":"14.6262687,121.0051642",
+				 "phone":"732-1595"
+				},
+			    {"dealer":"Grandprix Tire Supply",
+				 "add":"755 Edsa, Pasay City",
+				 "coor":"14.59585,121.0592585",
+				 "phone":"833-67-61 / 804-06-94"
+				}
+			  ];
